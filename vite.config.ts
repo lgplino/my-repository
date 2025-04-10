7590e9bf12aa5139c851bc0b1a74eb5ecacab719
lgplino <lucasmizukawa@gmail.com>
2025-04-10 00:12:09 -0300

    feat: add deploy script to package.json for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/<nome-do-repositorio>/', // Substitua <nome-do-repositorio> pelo nome do seu repositório
   plugins: [react()],
 })
