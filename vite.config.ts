786f39937a7450c608fb2387f01cdd55084afb39
lgplino <lucasmizukawa@gmail.com>
2025-04-10 00:23:47 -0300

    fix: update homepage in package.json and adjust base path in vite.config.ts for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<my-repository>/', // Substitua <nome-do-repositorio> pelo nome do seu repositório
   plugins: [react()],
+  base: "/my-repository",
 })
